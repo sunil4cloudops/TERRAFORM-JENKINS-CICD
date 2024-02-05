1d2d7f07080e938bc7d3577d4c772bab8605223e
sunil4cloudops <158776029+sunil4cloudops@users.noreply.github.com>
2024-02-05 16:40:49 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "cloudopsjenkins"
-    region         = "US East (Ohio) us-east-2"
+    bucket_key     = "cloudops/main"
+    region         = "us-east-2"
     dynamodb_table = "cloudops-dynamodb-table"
   }
 }
